d42ec5e486df0597ef86dfa036349f7a50b8d03c
rikarunze <rika.run@gmail.com>
2026-06-04 11:41:33 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+# ⚠️ เอา Token ที่ก๊อปเก็บไว้ตอนแรกมาใส่ตรงนี้ 
+# (ให้มันอยู่ในเครื่องหมาย ' ' เหมือนเดิมนะเว้ย)
+TOKEN = 'MTUxMTkxMDM4MTk3MzU0MDkzNQ.GTPO8K.KQzP8b3gS5uz15Mt0wdCZ5f3ljJ9E7VYJ0yxKA'
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'✅ ยัยตัวร้ายรายงานตัว! บอทออนไลน์แล้วเว้ยยย!')
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send("เข้ามาสิงแล้วจ้าาา 👻")
+    else:
+        await ctx.send("แกต้องเข้าห้องเสียงก่อนดิโว้ย ถึงจะเรียกชั้นเข้าไปได้!")
+
+bot.run(TOKEN)
